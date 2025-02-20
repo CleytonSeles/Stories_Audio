0ea8180f1b5f4c1a179fa682cb67fe5f6e8cc7e9
cleytonseles <cleyttonselestj@gmail.com>
2025-02-20 16:10:10 -0300

    configura o microserviço de gateway/API (gateway-service)

---
+++ Backend/gateway-service/server.js
@@ -0,0 +1,26 @@
+const fastify = require('fastify')({ logger: true });
+
+// Proxy para auth-service
+fastify.register(require('fastify-http-proxy'), {
+  upstream: 'http://localhost:3001',
+  prefix: '/auth', // todas as rotas de /auth serão redirecionadas para auth-service
+  rewritePrefix: '/auth',
+});
+
+// Proxy para audio-service
+fastify.register(require('fastify-http-proxy'), {
+  upstream: 'http://localhost:3002',
+  prefix: '/audio', // todas as rotas de /audio serão redirecionadas para audio-service
+  rewritePrefix: '/audio',
+});
+
+const start = async () => {
+  try {
+    await fastify.listen(3000);
+    fastify.log.info(`Server listening on ${fastify.server.address().port}`);
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+start();
